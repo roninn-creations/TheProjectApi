27b04232fc74688359f8f1b97c1cc17c526fba9c
dan <dan.tarsoly@gmail.com>
2019-05-06 17:42:07 +0200

    Help page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,42 @@
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-    return res.status(200).json({ message:'Welcome to the-project-api' });
+    const help = {
+        message: 'Welcome to the-project-api',
+        routes: {
+            places: [
+                'POST   ~/places      : Create new place',
+                'GET    ~/places      : Read all the places',
+                'GET    ~/places/{id} : Read the place with given ID',
+                'PUT    ~/places/{id} : Update the place with given ID',
+                'DELETE ~/places/{id} : Delete the place with given ID'
+            ],
+            users: [
+                'POST   ~/users      : Create new user',
+                'GET    ~/users      : Read all the users',
+                'GET    ~/users/{id} : Read the user with given ID',
+                'PUT    ~/users/{id} : Update the user with given ID',
+                'DELETE ~/users/{id} : Delete the user with given ID'
+            ],
+            reviews: [
+                'POST   ~/reviews      : Create new review',
+                'GET    ~/reviews      : Read all the reviews',
+                'GET    ~/reviews/{id} : Read the review with given ID',
+                'PUT    ~/reviews/{id} : Update the review with given ID',
+                'DELETE ~/reviews/{id} : Delete the review with given ID'
+            ],
+            auth: [
+                'POST   ~/auth                   : Register user with email and password',
+                'GET    ~/auth                   : Get current user info ',
+                'GET    ~/auth/basic             : Sign in with email and password',
+                'GET    ~/auth/google            : Sign in/up with Google',
+                'GET    ~/auth/google/callback   : Callback URL for Google login',
+                'GET    ~/auth/facebook          : Sign in/up with Facebook',
+                'GET    ~/auth/facebook/callback : Callback URL for Facebook login'
+            ],
+        }
+    };
+    return res.status(200).json( help );
 });
 
 module.exports = router;
