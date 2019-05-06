54ad6e563485e46160d23184e5329693e3f5c043
dan <dan.tarsoly@gmail.com>
2019-05-06 16:47:36 +0200

    Deploying to Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-    return res.status(200).json({message:'welcome to the-project-api'});
+    return res.status(200).json({ message:'Welcome to the-project-api' });
 });
 
 module.exports = router;
